ff608358f9fcbb3fd7f98d27f2bd1eb151d6d432
MCCR-19 <cariolato.michael@gmail.com>
2025-06-05 17:55:55 +0200

    fix grafica

--- Scatola_Cultura_2025/src/interfaces/IDisabilitàStruttura.ts
+++ Scatola_Cultura_2025/src/interfaces/IDisabilitàStruttura.ts
@@ -1,7 +1,7 @@
  export interface DisabilitaStruttura {
   disabilitaStruttura: number;
   categoria: string;
-  //idStruttura:number;
+  idStruttura:number;
   descrizione: string;
   dataInserimento: Date;
   flgDisabilita: boolean;
