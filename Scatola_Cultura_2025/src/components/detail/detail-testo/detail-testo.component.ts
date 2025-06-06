85e06e312845e07488e71273fae3908f93ad613c
MCCR-19 <cariolato.michael@gmail.com>
2025-06-06 14:45:31 +0200

    gestione icone

--- Scatola_Cultura_2025/src/components/detail/detail-testo/detail-testo.component.ts
+++ Scatola_Cultura_2025/src/components/detail/detail-testo/detail-testo.component.ts
@@ -1,26 +1,33 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input,  Output } from '@angular/core';
 import { IconeManager } from '../IconeManager';
+import { DisabilitaStruttura } from '../../../interfaces/IDisabilitàStruttura';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-detail-testo',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './detail-testo.component.html',
   styleUrl: './detail-testo.component.css'
 })
-export class DetailTestoComponent {
+export class DetailTestoComponent{
 
   @Input() testo:string='';
+  @Input() accessibilita:DisabilitaStruttura[]|null=null;
 
   @Output() zoomClicked=new EventEmitter<string>();
 
-  constructor(private iconeManager:IconeManager){}
+  hasAccessibilita:boolean=false;
 
+  constructor(private iconeManager:IconeManager, private sanitizer:DomSanitizer){}
+  
   Icone=this.iconeManager
   
   onZoomClick(){
-    this.zoomClicked.emit(this.testo)
+      this.zoomClicked.emit(this.testo)
   }
+
 
 
 
