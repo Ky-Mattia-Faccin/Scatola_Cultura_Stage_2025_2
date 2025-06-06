56e6e237196f979df85a716713e3d275897d2d1a
Mattia Faccin <mattia@kymos.eu>
2025-06-06 12:14:56 +0200

    Routes

--- Scatola_Cultura_2025/src/components/app/app.routes.ts
+++ Scatola_Cultura_2025/src/components/app/app.routes.ts
@@ -1,7 +1,18 @@
 import { Routes } from '@angular/router';
+import { Homepage } from '../homepage/homepage';
+import { Detail } from '../detail/detail';
 
 export const routes: Routes = [
-    {
-        
+    { 
+        path: '', 
+        component: Homepage 
+    },
+    { 
+        path: 'detail',
+        component: Detail 
+    },
+    { 
+        path: 'homepage', 
+        component: Homepage 
     }
 ];
