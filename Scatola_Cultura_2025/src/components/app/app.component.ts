d84a3f746e80a945b9fd98aad07a6433e498a40d
Mattia Faccin <mattia@kymos.eu>
2025-06-06 12:13:40 +0200

    Commit incompleto

--- Scatola_Cultura_2025/src/components/app/app.component.ts
+++ Scatola_Cultura_2025/src/components/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Homepage } from '../homepage/homepage';
+import { Detail } from '../detail/detail';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Homepage],
+  imports: [RouterOutlet, Homepage, Detail],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
