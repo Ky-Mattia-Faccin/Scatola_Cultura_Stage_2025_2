27e924e7fc2338d6b24f0a9c5809bd177a9c7055
MCCR-19 <cariolato.michael@gmail.com>
2025-06-09 17:39:22 +0200

    aggiunta social e funzionalità per luminosità e contrasto

--- Scatola_Cultura_2025/src/components/nav-bar/nav-bar.component.ts
+++ Scatola_Cultura_2025/src/components/nav-bar/nav-bar.component.ts
@@ -1,20 +1,30 @@
-import { Component, HostListener, ViewChild } from '@angular/core';
+import { Component, EventEmitter, HostListener, Output, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css',
 })
 export class NavBarComponent {
 
+ brightness!:number
+ contrast!:number
+
+ 
+ @Output() brightnessChanged = new EventEmitter<number>();
+ @Output() contrastChanged = new EventEmitter<number>();
   
   toggleMenu() {
     const dropDownMenu = document.querySelector('.sc-navbar-dropdown-menu');
     dropDownMenu?.classList.toggle('hidden');
   }
 
+  applyFilter(){
+
+  }
   
 
 }
