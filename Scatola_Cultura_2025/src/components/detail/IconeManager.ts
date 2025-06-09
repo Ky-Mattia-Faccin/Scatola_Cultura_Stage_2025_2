f6054d0ee51de0afca6322da3c38a42f1e971805
MCCR-19 <cariolato.michael@gmail.com>
2025-06-09 13:54:36 +0200

    passaggio dati

--- Scatola_Cultura_2025/src/components/detail/IconeManager.ts
+++ Scatola_Cultura_2025/src/components/detail/IconeManager.ts
@@ -46,14 +46,14 @@ export class IconeManager{
         d="M3 6.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5"
       />
     </svg>`,
-  Mobilità: `
+  mobilità: `
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-wheelchair" viewBox="0 0 16 16">
       <path d="M12 3a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3m-.663 2.146a1.5 1.5 0 0 0-.47-2.115l-2.5-1.508a1.5 1.5 0 0 0-1.676.086l-2.329 1.75a.866.866 0 0 0 1.051 1.375L7.361 3.37l.922.71-2.038 2.445A4.73 4.73 0 0 0 2.628 7.67l1.064 1.065a3.25 3.25 0 0 1 4.574 4.574l1.064 1.063a4.73 4.73 0 0 0 1.09-3.998l1.043-.292-.187 2.991a.872.872 0 1 0 1.741.098l.206-4.121A1 1 0 0 0 12.224 8h-2.79zM3.023 9.48a3.25 3.25 0 0 0 4.496 4.496l1.077 1.077a4.75 4.75 0 0 1-6.65-6.65z"/>
       </svg>`,
-  Udito: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-ear-fill" viewBox="0 0 16 16">
+  udito: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-ear-fill" viewBox="0 0 16 16">
         <path d="M8.5 0A5.5 5.5 0 0 0 3 5.5v7.047a3.453 3.453 0 0 0 6.687 1.212l.51-1.363a4.6 4.6 0 0 1 .67-1.197l2.008-2.581A5.34 5.34 0 0 0 8.66 0zM7 5.5v2.695q.168-.09.332-.192c.327-.208.577-.44.72-.727a.5.5 0 1 1 .895.448c-.256.513-.673.865-1.079 1.123A9 9 0 0 1 7 9.313V11.5a.5.5 0 0 1-1 0v-6a2.5 2.5 0 0 1 5 0V6a.5.5 0 0 1-1 0v-.5a1.5 1.5 0 1 0-3 0"/>
         </svg>`,
-  Vista: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16"><path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0"/><path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7"/>
+  vista: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16"><path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0"/><path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7"/>
       </svg>
       `,
   Cognitiva: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-book" viewBox="0 0 16 16">
--- Scatola_Cultura_2025/src/components/homepage/homepage.ts
+++ Scatola_Cultura_2025/src/components/homepage/homepage.ts
@@ -1,6 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+
+import { TbStruttura } from '../../strutture/TbStruttura';
+import { TbDisabilitaStruttura } from '../../strutture/tbDisabilitàStruttura'
+import { TbDisabilitaCategorie } from '../../strutture/tbDisabilitaCategorie';
+import { Struttura } from '../../interfaces/Istruttura';
 
 @Component({
   standalone:true,
@@ -11,26 +16,15 @@ import { RouterModule } from '@angular/router';
 })
 
 export class Homepage {
-  items = [
-    {nome: 'Musei Vaticani - Città del Vaticano', immagine: 'assets/prova.avif'},
-    {nome: 'Galleria degli Uffizi - Firenze', immagine: 'assets/esempio.jpg'},
-    {nome:'Museo Egizio - Torino', immagine: 'assets/prova.avif'},
-    {nome:'Galleria dell\'Accademia - Firenze',immagine: 'assets/prova.avif'},
-    {nome:'Museo Nazionale di Castel Sant\'Angelo - Roma', immagine: 'assets/prova.avif'},
-    {nome:'Museo Nazionale Archeologico - Napoli', immagine: 'assets/prova.avif'},
-    {nome:'Palazzo Pitti - Firenze', immagine: 'assets/prova.avif'},
-    {nome:'MAXXI - Museo Nazionale delle Arti del XXI Secolo - Roma',immagine: 'assets/prova.avif'},
-    {nome: 'Museo dell\'Opera del Duomo - Firenze', immagine: 'assets/prova.avif'},
-    {nome:'Museo Nazionale Romano - Roma', immagine: 'assets/prova.avif'},
-    {nome:'Museo Civico di Castelbuono - Castelbuono', immagine: 'assets/prova.avif'},
-    {nome:'Museo Galileo - Firenze', immagine: 'assets/prova.avif'},
-    {nome:'Triennale Design Museum - Milano',immagine: 'assets/prova.avif'},
-    {nome:'Museo del Novecento - Milano',immagine: 'assets/prova.avif'},
-    {nome:'Museo di Capodimonte - Napoli', immagine: 'assets/prova.avif'},
-    {nome:'Peggy Guggenheim Collection - Venezia',immagine: 'assets/prova.avif'},
-    {nome:'Gallerie dell\'Accademia - Venezia',immagine: 'assets/prova.avif'},
-    {nome:'Museo Archeologico Nazionale - Taranto', immagine: 'assets/prova.avif'},
-    {nome:'Museo Storico Italiano della Guerra - Rovereto',immagine: 'assets/prova.avif'},
-    {nome:'Museo Ferrari - Maranello',immagine: 'assets/prova.avif'}
-  ];
+
+  strutture:Struttura[]=[];
+
+  
+
+  ngOnInit(): void {
+    this.strutture = JSON.parse(localStorage.getItem('strutture') || '[]');
+  }
+
+
+  
 }
