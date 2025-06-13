9f27544176a0456711b508d38106c4a10ae083bc
negrosimo1702ne@gmail.com <negrosimo1207ne@gmail.com>
2025-06-13 08:45:29 +0200

    cancellazione codice inutilizzato

--- Scatola_Cultura_2025/src/services/search-filter.service.ts
+++ Scatola_Cultura_2025/src/services/search-filter.service.ts
@@ -32,12 +32,4 @@ export class SearchFilterService {
   setSearchFilter(filtro:string){
     this.oggettoRicerca.next(filtro)
   }
-
-  //Dimensione testi 
-  private textSizeSubject = new BehaviorSubject<number>(16); // default iniziale    
-  textSize$ = this.textSizeSubject.asObservable();
-   
-  setTextSize(size: number) {
-    this.textSizeSubject.next(size);
-  }
 }
