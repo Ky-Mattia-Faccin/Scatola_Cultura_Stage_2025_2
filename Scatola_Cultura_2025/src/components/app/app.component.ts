81c5c89181b073fcca841ee8a945b632f1a6127a
negrosimo1702ne@gmail.com <negrosimo1207ne@gmail.com>
2025-06-26 17:14:33 +0200

    cor tabulazioni

--- Scatola_Cultura_2025/src/components/app/app.component.ts
+++ Scatola_Cultura_2025/src/components/app/app.component.ts
@@ -2,19 +2,19 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, RouterOutlet } from '@angular/router';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { Struttura } from '../../interfaces/Istruttura';
-import {  Observable} from 'rxjs';
+import { Observable } from 'rxjs';
 import { SearchFilterService } from '../../services/search-filter.service';
 import { HttpClient } from '@angular/common/http';
-import {  slideAnimation } from './route-transition';
+import { slideAnimation } from './route-transition';
 import { StrutturaService } from '../../services/struttura.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  animations:[slideAnimation]
+  animations: [slideAnimation]
 })
 export class AppComponent {
 
@@ -25,14 +25,14 @@ export class AppComponent {
 
   // Valori dei filtri di luminosità e contrasto (di default 100%), valore di text size (Default:18)
   brightness: number = 100;
-  contrast: number = 100; 
+  contrast: number = 100;
 
   // Array per contenere le strutture caricate dal file JSON/localStorage
   strutture: Struttura[] = [];
 
   // Iniezione del servizio HttpClient per effettuare richieste HTTP
   //iniezione del servizio ActivatedRoute per conoscere la rotta attiva (utilizzato per l'animazione)
-  constructor(private httpClient: HttpClient, protected route:ActivatedRoute,private servizioStruttura:StrutturaService) {}
+  constructor(private httpClient: HttpClient, protected route: ActivatedRoute, private servizioStruttura: StrutturaService) { }
 
   /*
    * Applica i filtri di luminosità e contrasto alla pagina:
